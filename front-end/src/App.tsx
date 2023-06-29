c0ddb474acdf3bb657feb4f9f0996abc0dd3bacc
Luca <james.lol2000@gmail.com>
2023-06-29 15:50:57 +0200

    add front end

---
+++ front-end/src/App.tsx
@@ -0,0 +1,13 @@
+import Sidebar from "./scenes/Sidebar";
+import { Box } from "@mui/material";
+
+function App() {
+  return (
+    <Box>
+      <Sidebar />
+      
+    </Box>
+  );
+}
+
+export default App;
